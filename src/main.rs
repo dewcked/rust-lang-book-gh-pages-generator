22120191b3d6c8a99f746400c6e8f8366c95f168
dewcked <dewcked@protonmail.ch>
2022-12-23 00:34:00 +0900

    Try Listing .md files in book/src

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ use std::env;
 fn main() -> std::io::Result<()> {
     let dir_entries = env::current_dir()?.join("book").join("src").read_dir()?;
     for dir_entry in dir_entries {
-        println!("{:?}", dir_entry?.file_name());
+        // match dir_entry?.file_type()
+        match dir_entry?.file_name().to_str() {
+            Some(dir_name) => {
+                if dir_name.ends_with(".md") {
+                    println!("{}", dir_name)
+                }
+            }
+            None => {}
+        }
+        // println!("{}", dir_entry?.file_name().to_str().un);
     }
     Ok(())
 }
